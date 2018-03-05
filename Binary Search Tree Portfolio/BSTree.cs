4996bf60b1e99427aa0780198aeca2e6bdf0b435
Sean O'Mahoney <seandavidomahoney@gmail.com>
2018-03-05 14:05:49 +0000

    init

---
+++ Binary Search Tree Portfolio/BSTree.cs
@@ -0,0 +1,301 @@
+﻿using System;
+
+namespace Binary_Search_Tree
+{
+    class BSTree<T> : BinTree<T> where T : IComparable
+    {
+        public int Height()
+        {
+            return height(root);
+        }
+
+        protected int height(Node<T> tree)
+        {
+            if (tree == null) return 0;
+            else return 1 + Math.Max(height(tree.Left), height(tree.Right));
+        }
+
+        public int Count()
+        {
+            return count(root);
+        }
+
+        private int count(Node<T> tree)
+        {
+            if (tree == null) return 0;
+            return (1 + (count(tree.Left)) + (count(tree.Right)));
+        }
+        
+        public Boolean Contains(T item)
+        {
+            return contains(item, ref root);
+        }
+
+        private Boolean contains(T item, ref Node<T> root)
+        {
+            if (root != null)
+            {
+                if (item.CompareTo(root.Data) == 0) return true;
+                else if (item.CompareTo(root.Data) < 0 | (item.CompareTo(root.Data) > 0)) return contains(item, ref root.Left) | contains(item, ref root.Right);
+            } return false;
+        }
+
+        public void RemoveItem(T item)
+        {
+            removeItem(item, ref root);
+        }
+
+        public void removeItem(T item, ref Node<T> tree)
+        {
+            if (Contains(item))
+            {
+                if (tree != null)
+                {
+                    if (item.CompareTo(tree.Data) < 0)
+                    {
+                        removeItem(item, ref tree.Left);
+                    }
+                    else if (item.CompareTo(tree.Data) > 0)
+                    {
+                        removeItem(item, ref tree.Right);
+                    }
+                    else
+                    {
+                        if (tree.Left == null)
+                        {
+                            tree = tree.Right;
+                        }
+                        else if (tree.Right == null)
+                        {
+                            tree = tree.Left;
+                        }
+                        else
+                        {
+                            T root = leastItem(tree.Right);
+                            tree.Data = root;
+                            removeItem(tree.Data, ref tree.Right);
+                        }
+                    }
+                }
+            }
+        }
+        
+        public T leastItem(Node<T> tree)
+        {
+            if (tree.Left == null) return tree.Data;
+            else return leastItem(tree.Left);
+        }
+
+        public void InsertItem(T item)
+        {
+            insertItem(item, ref root);
+        }
+
+        private void insertItem(T item, ref Node<T> tree)
+        {
+            if (tree == null)
+                tree = new Node<T>(item);
+            else if (item.CompareTo(tree.Data) < 0)
+                insertItem(item, ref tree.Left);
+            else if (item.CompareTo(tree.Data) > 0)
+                insertItem(item, ref tree.Right);
+        }
+
+    }
+
+    /*
+    class BSTree<T> : BinTree<T> where T : IComparable
+    {
+
+        public BSTree()
+        {
+            root = null;
+        }
+
+        public BSTree(Node<T> node)
+        {
+            root = node;
+        }
+
+        public void InsertItem(T item)
+        {
+            insertItem(item, ref root);
+        }
+
+        private void insertItem(T item, ref Node<T> tree)
+        {
+            if (tree == null)
+                tree = new Node<T>(item);
+
+            else if (item.CompareTo(tree.Data) < 0)
+                insertItem(item, ref tree.Left);
+
+            else if (item.CompareTo(tree.Data) > 0)
+                insertItem(item, ref tree.Right);
+        }
+
+        public int Height(Node<T> tree)
+        {
+            if (tree == null)
+                return 0;
+            else
+            {
+                return
+                1 + Math.Max(Height(tree.Left), Height(tree.Right));
+            }
+
+        }
+
+        public int Count()
+        {
+            int Count1 = 0;
+            return count(root, ref Count1);
+        }
+        private int count(Node<T> tree, ref int Count1)
+        {
+            if (tree != null)
+            {
+
+                count(tree.Left, ref Count1);
+                count(tree.Right, ref Count1);
+                Count1++;
+
+            }
+            return Count1;
+        }
+
+        public Boolean Contains(T item)
+        {
+            return contains(item, ref root);
+        }
+
+        private Boolean contains(T item, ref Node<T> root)
+        {
+            if (root != null) {
+                if (item.CompareTo(root.Data) == 0) return true;
+                else if (item.CompareTo(root.Data) < 0 | (item.CompareTo(root.Data) > 0)) return contains(item, ref root.Left) | contains(item, ref root.Right);
+            } return false;
+        }
+
+        public void RemoveItem(T item)
+        {
+            removeItem(item, ref root);
+        }
+
+        public void removeItem(T item, ref Node<T> tree)
+        {
+            if (tree != null) {
+                if (item.CompareTo(tree.Data) < 0)
+                {
+                    removeItem(item, ref tree.Left);
+                }
+                else if (item.CompareTo(tree.Data) > 0)
+                {
+                    removeItem(item, ref tree.Right);
+                }
+                else
+                {
+                    if (tree.Left == null)
+                    {
+                        tree = tree.Right;
+                    }
+                    else if (tree.Right == null)
+                    {
+                        tree = tree.Left;
+                    }
+                    else
+                    {
+                        T root = leastItem(tree.Right);
+                        tree.Data = root;
+                        removeItem(tree.Data, ref tree.Right);
+                    }
+                }
+            }
+        }
+
+        public T leastItem(Node<T> tree)
+        {
+            if (tree.Left == null) return tree.Data;
+            else return leastItem(tree.Left);
+        }
+
+        /*
+        public void RemoveItem(T item)
+        {
+            removeItem(item, ref root);
+        }
+
+        private void removeItem(T item, ref Node<T> tree)
+        {
+                if (tree != null)
+                {
+                    if (item.CompareTo(tree.Data) < 0)
+                    {
+                        removeItem(item, ref tree.Left);
+                    }
+                    else if (item.CompareTo(tree.Data) > 0)
+                    {
+                        removeItem(item, ref tree.Right);
+                    }
+                    else
+                    {
+                        if (tree.Left == null)
+                        {
+                            tree = tree.Right;
+                        }
+                        else if (tree.Right == null)
+                        {
+                            tree = tree.Left;
+                        }
+                        else
+                        {
+                            T root = leastItem(tree.Right);
+                            tree.Data = root;
+                            removeItem(tree.Data, ref tree.Right);
+                        }
+                    }
+                }
+        }
+
+        /*
+        private void removeItem(T item, ref Node<T> tree)
+        {
+            //BSTree<T> temp = new BSTree<T>(tree);
+            if (tree == null) return;
+            else if (item.CompareTo(tree.Data) < 0) removeItem(item, ref tree.Left);
+            else if (item.CompareTo(tree.Data) > 0) removeItem(item, ref tree.Right);
+            else if (item.CompareTo(tree.Data) == 0)
+            {
+                if (tree.Right == null && tree.Left == null)
+                {
+                    tree = null;
+                }
+                if (tree.Left == null)
+                {
+                    //Node<T> t = tree;
+                    tree = tree.Right;
+                }
+                if (tree.Right == null)
+                {
+                    //Node<T> t = tree;
+                    tree = tree.Left;
+                }
+                if (tree.Right != null && tree.Left != null)
+                {
+                    tree.Data = leastItem(tree.Right);
+                    removeItem(tree.Data, ref tree);
+                    //temp.root.Data = leastItem(temp.root.Right);
+                    //temp.removeItem(temp.root.Data, ref temp.root);
+                }
+            }
+        }
+        
+        private T leastItem(Node<T> tree)
+        {
+            if (tree.Left == null) return tree.Data;
+            else return leastItem(tree.Left);
+        }
+        
+    }
+    */
+}
---
+++ Binary Search Tree Portfolio/Program.cs
@@ -0,0 +1,96 @@
+﻿using Binary_Search_Tree_Portfolio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVLTree<int> tree = new AVLTree<int>();
+            // Node<int>[] nodes = { new Node<int>(10), new Node<int>(15), new Node<int>(17), new Node<int>(23), new Node<int>(25) };
+            tree.InsertItem(25);
+            tree.InsertItem(17);
+            tree.InsertItem(15);
+            tree.InsertItem(23);
+            tree.InsertItem(10);
+            String s = null;
+            tree.InOrder(ref s);
+
+            Console.WriteLine("AVL Height " + tree.Height());
+            Console.WriteLine("AVL InOrder " + s);
+
+            BSTree<int> tree2 = new BSTree<int>();
+            // Node<int>[] nodes = { new Node<int>(10), new Node<int>(15), new Node<int>(17), new Node<int>(23), new Node<int>(25) };
+            tree2.InsertItem(25);
+            tree2.InsertItem(17);
+            tree2.InsertItem(15);
+            tree2.InsertItem(23);
+            tree2.InsertItem(10);
+            String s2 = null;
+            tree2.InOrder(ref s2);
+
+            Console.WriteLine("BSTree Height " + tree2.Height());
+            Console.WriteLine("BSTree InOrder " + s2);
+            Console.ReadKey();
+
+            //Node<int>[] a =
+            //{
+            //    new Node<int>(1), new Node<int>(2), new Node<int>(3), new Node<int>(4), new Node<int>(5),
+            //    new Node<int>(6), new Node<int>(7), new Node<int>(8), new Node<int>(9), new Node<int>(10), new Node<int>(11)
+            //};
+            //a[0].Left = a[1];
+            //a[0].Right = a[2];
+            //a[1].Left = a[3];
+            //a[1].Right = a[4];
+            //a[2].Left = a[5];
+            //a[2].Right = a[6];
+            //a[4].Right = a[7];
+            //a[7].Left = a[8];
+            //a[7].Right = a[9];
+            //a[9].Right = a[10];
+
+            //BSTree<int> bt = new BSTree<int>();
+            //// for (int i = 0; i < 13; i++) bt.InsertItem(i);
+            //bt.InsertItem(5);
+            //bt.InsertItem(4);
+            //bt.InsertItem(8);
+            //bt.InsertItem(6);
+
+            //// HEIGHT
+            //Console.WriteLine("Height: " + bt.Height());
+
+            //// COUNT
+            //Console.WriteLine("Amount: " + bt.Count());
+
+            //String s1 = null;
+            //bt.PreOrder(ref s1);
+            //Console.WriteLine("BST: " + s1);
+
+            //// CONTAINS
+            //int[] n = new int[] { 8, 14 };
+            //foreach (int x in n)
+            //{
+            //    if (bt.Contains(x)) Console.WriteLine("BST Contains " + x);
+            //    else Console.WriteLine("BST Does Not Contains " + x);
+            //}
+
+            //// REMOVE
+            //int y = 5;
+            //bt.RemoveItem(y);
+            //if (bt.Contains(y)) Console.WriteLine("BST Contains " + y);
+            //else Console.WriteLine("BST Does Not Contains " + y);
+
+            //// HEIGHT
+            //Console.WriteLine("Height: " + bt.Height());
+
+            //String s2 = null;
+            //bt.PreOrder(ref s2);
+            //Console.WriteLine("BST: " + s);
+        }
+    }
+}
